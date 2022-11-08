46bcc80714fa56f4360ad9f4d530e3250b0dfdc5
Anton TiU <anton@olssoncollentine.com>
2022-11-08 09:31:41 +0100

    Clean data

--- code/create-plots.r
+++ code/create-plots.r
@@ -25,17 +25,87 @@ dat_split <- split(dat, dat$power)
 #i.e. one facet for each power level, 3 lines each
 
 #take the mean at each step of the counts
-dat_split2 <- lapply(dat_split, function(x){
-    x[, .(hyp0 = mean(x$count.researchers.with..hypothesis...0.),
-    hyp1 = mean(x$count.researchers.with..hypothesis...1.),
-    hyp2 = mean(x$count.researchers.with..hypothesis...2.)),
-    by = .(X.step.)]
-})
 
+test <- dat_split[[10]]
 test <- setDT(dat_split[[10]])
-test[, .(hyp0 = mean(test$count.researchers.with..hypothesis...0.),
-    hyp1 = mean(test$count.researchers.with..hypothesis...1.),
-    hyp2 = mean(test$count.researchers.with..hypothesis...2.)),
-    by = test$X.step.]
+names(test) <- gsub("\\.", "_", names(test))
+names(test)[8:10] <- c("hyp0", "hyp1", "hyp2")
 
-#Not working.
+test[, .(mean_hyp0 = mean(hyp0),
+        mean_hyp1 = mean(hyp1),
+        mean_hyp2 = mean(hyp2)),
+        by = X_step_]
+
+#both work
+
+#tidyverse
+library(dplyr)
+test2 <- test %>% group_by(X_step_) %>%
+summarize(hyp0 = mean(hyp0),
+          hyp1 = mean(hyp1))
+
+
+#works
+#Now apply to list
+
+#Above section as a function
+get_means <- function(x){
+    x <- setDT(x)
+    names(x) <- gsub("\\.", "_", names(x)) #get rid of strange .names
+    names(x)[8:10] <- c("hyp0", "hyp1", "hyp2")
+
+    x[, .(mean_hyp0 = mean(hyp0), #out
+        mean_hyp1 = mean(hyp1),
+        mean_hyp2 = mean(hyp2)),
+        by = X_step_]
+}
+
+#apply to list
+dat_split2 <- lapply(dat_split, get_means)
+
+#recombine
+dat2 <- data.table::rbindlist(dat_split2,
+                              idcol = "power")
+
+#make wide to long
+dat3 <- data.table::melt(dat2,
+        id = c("power", "X_step_"),
+        measure.vars = c("mean_hyp0",
+                        "mean_hyp1",
+                        "mean_hyp2"))
+
+#Now ready for plotting
+library(ggplot2)
+
+ggplot(dat3) +
+geom_line(aes(x = X_step_, y = value,
+ color = variable), alpha = 0.8) +
+ facet_wrap(~power)
+
+#zoom in
+dat_few_ticks <- dat3[dat3$X_step_ < 50,]
+
+ggplot(dat_few_ticks) +
+geom_line(aes(x = X_step_, y = value,
+ color = variable), alpha = 0.8) +
+ facet_wrap(~power)
+
+#zoom in more
+dat_30_ticks <- dat3[dat3$X_step_ < 30,]
+
+ggplot(dat_30_ticks) +
+geom_line(aes(x = X_step_, y = value,
+ color = variable), alpha = 0.3) +
+ facet_wrap(~power) +
+ xlab("Rounds of studies") +
+ ylab("Researcher per hypothesis") +
+ ggtitle("Researchers per topic for different true effect size")
+
+
+#Need to think about whether the mean is really a good summary
+# basically, any row with 0 doesn't exist, so the mean
+#Becomes whatever did survive longer
+#I would need to set all remaining rows to zero for runs
+#that did not go further than some tick below 200
+
+#Hmm, this will still end up in a value larger than zero
